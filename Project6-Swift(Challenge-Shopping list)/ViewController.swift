9b6d098abe9b9365c63e5facd1a6940a9439c6a0
Ismail <a.isumairu01@gmail.com>
2022-02-16 19:08:05 +0300

    added answers submition, removed extra empty cells

--- Project6-Swift(Challenge-Shopping list)/ViewController.swift
+++ Project6-Swift(Challenge-Shopping list)/ViewController.swift
@@ -15,13 +15,44 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Shopping List"
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        tableView.tableFooterView = UIView()
     }
 
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter product", message: nil, preferredStyle: .alert)
         ac.addTextField()
+    
+    let submitAction = UIAlertAction(title: "Submit", style: .default) {
+        [weak self, weak ac] action in
+        guard let answer = ac?.textFields?[0].text else { return }
+        self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
     }
 
+    func submit(_ answer: String) {
+        shoppingList.insert(answer, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        return
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
 }
 
